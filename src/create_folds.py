e0a91cb304bf32a0e8fbf77a6a34f4b94002279e
sudsfsp <sudhakar.sfsp@gmail.com>
2020-05-21 20:13:18 +0530

    divided data into 5 kfold

--- src/create_folds.py
+++ src/create_folds.py
@@ -1,9 +1,16 @@
+import pandas as pd
+from sklearn import model_selection
 
-def add(a,b):
-    return(a+b)
 
+if __name__=="__main__":
+    df = pd.read_csv("input/train.csv")
+    df["kfold"] = -1
 
-def multiply(a,b):
-    return(a*b)
-def subtract(a,b):
-    return(a-b)
+    df = df.sample(frac=1).reset_index(drop=True)
+    kf = model_selection.StratifiedKFold(n_splits=5, shuffle=False, random_state=42)
+
+    for fold, (train_idx, valid_idx) in enumerate(kf.split(X=df, y=df.target.values)):
+        print(len(train_idx), len(valid_idx))
+        df.loc[valid_idx, 'kfold'] = fold
+
+        df.to_csv("input/train_folds.csv", index=False)
